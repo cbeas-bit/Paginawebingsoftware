88c2978510d2eb0b3d636af3a5574ed03e7120f7
cbeas-bit <cbeas@utem.cl>
2026-06-25 16:45:08 -0400

    mejora para concción entre paneles
    conectar el panel admin con supabase para subir las fotos, se modifico el archivo js/supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,5 +4,12 @@
 // ============================================================
 const SUPABASE_URL      = "https://mvtdzqjtbmgvhdoouhjw.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dGR6cWp0Ym1ndmhkb291aGp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxMzE4OTYsImV4cCI6MjA5NzcwNzg5Nn0.8oECgc0FHwmjwinCISL4tsaZkQP75nfi2QHPHKivIUA";
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY){
+    auth: {
+        persistSession: true,
+        storageKey: 'mujercobra-admin',
+        storage: window.localStorage,
+        autoRefreshToken: true,
+    }
+});
 window.supabaseClient = supabaseClient;
