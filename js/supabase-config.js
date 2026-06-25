7e4deb01c554fce77ff78ab6e8b253c1c00ae34b
cbeas-bit <cbeas@utem.cl>
2026-06-25 18:01:21 -0400

    supabase
    corrección de comas

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,7 +4,8 @@
 // ============================================================
 const SUPABASE_URL      = "https://mvtdzqjtbmgvhdoouhjw.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dGR6cWp0Ym1ndmhkb291aGp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxMzE4OTYsImV4cCI6MjA5NzcwNzg5Nn0.8oECgc0FHwmjwinCISL4tsaZkQP75nfi2QHPHKivIUA";
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY){
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     auth: {
         persistSession: true,
         storageKey: 'mujercobra-admin',
